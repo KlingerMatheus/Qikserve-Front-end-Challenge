f5ae84ac1d368ce6aa2f3b0cbeb2f099f63c8eab
Klinger Matheus <kger.matheus@gmail.com>
2025-02-27 15:48:22 -0300

    feat: build 'Navbar' component.

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,69 @@
+import { FC, useState } from "react";
+import { NAVBAR_ITEMS } from "./constants";
+import { useBreakpoints, useNavigate } from "@/hooks/index";
+import { CloseIcon, MenuIcon } from "@/assets/icons";
+import "./style.css";
+import { createPortal } from "react-dom";
+
+export const Navbar: FC = () => {
+  const { route } = useNavigate();
+  const device = useBreakpoints();
+  const isMobileOrTablet = ["mobile", "tablet"].includes(device);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="navbar">
+        {isMobileOrTablet && (
+          <>
+            <div />
+            <h1 className="label">Menu</h1>
+            <button
+              className="icon-container"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <MenuIcon />
+            </button>
+          </>
+        )}
+
+        {!isMobileOrTablet &&
+          NAVBAR_ITEMS.map((item) => {
+            const isActive = route === item.path;
+
+            return (
+              <li key={item.name} className={isActive ? "active" : ""}>
+                <a href={item.path}>{item.name}</a>
+                {isActive && <span />}
+              </li>
+            );
+          })}
+      </nav>
+
+      {isMenuOpen &&
+        createPortal(
+          <nav className="mobile-navbar">
+            <button
+              className="close-button-icon"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <CloseIcon color="gray" aria-label="Close Menu" />
+            </button>
+            <div className="mobile-navbar-items">
+              {NAVBAR_ITEMS.map((item, index) => {
+                const isActive = route === item.path;
+
+                return (
+                  <li key={item.name} className={`item delay- ${index}`}>
+                    <a href={item.path}>{item.name}</a>
+                    {isActive && <span />}
+                  </li>
+                );
+              })}
+            </div>
+          </nav>,
+          document.body
+        )}
+    </>
+  );
+};
