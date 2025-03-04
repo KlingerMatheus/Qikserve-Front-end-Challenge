49d4793b1a3fb89db9e39aab8d5100c7ecf2e626
Klinger Matheus <kger.matheus@gmail.com>
2025-03-04 09:38:35 -0300

    chore: implement vite and ts config.

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    define: {
+      "process.env": env,
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://cdn-dev.preoday.com/challenge",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  });
+};
