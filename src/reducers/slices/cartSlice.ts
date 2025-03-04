a66d5cfed2bb42c79bcc31853fa66c681f8d51d8
Klinger Matheus <kger.matheus@gmail.com>
2025-03-03 23:22:12 -0300

    refactor: improves code legibility

--- src/reducers/slices/cartSlice.ts
+++ src/reducers/slices/cartSlice.ts
@@ -12,11 +12,60 @@ const initialState: CartState = {
   selectedItem: undefined,
 };
 
+function itemExists(state: CartState, item: CartItem): CartItem | undefined {
+  return state.items.find(
+    (cartItem) =>
+      cartItem.id === item.id &&
+      cartItem.selectedModifierId === item.selectedModifierId
+  );
+}
+
+function updateExistingItem(state: CartState, action: PayloadAction<CartItem>) {
+  const { id, selectedModifierId, quantity } = action.payload;
+
+  cartSlice.caseReducers.updateCartItemQuantity(state, {
+    type: cartSlice.actions.updateCartItemQuantity.type,
+    payload: {
+      id,
+      quantity,
+      type: "customIncrement",
+      modifierId: selectedModifierId,
+    },
+  });
+}
+
+function updateQuantity(
+  item: CartItem,
+  action: PayloadAction<UpdateQuantityAction>
+): number {
+  const quantityMap: {
+    [key: string]: (
+      item: CartItem,
+      action: PayloadAction<UpdateQuantityAction>
+    ) => number;
+  } = {
+    customIncrement: (item, action) =>
+      item.quantity + (action.payload.quantity || 0),
+    decrement: (item) => item.quantity - 1,
+    increment: (item) => item.quantity + 1,
+  };
+
+  const updateFn = quantityMap[action.payload.type];
+  return updateFn ? updateFn(item, action) : item.quantity;
+}
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     cartAddNewItem: (state, action: PayloadAction<CartItem>) => {
+      const existingItem = itemExists(state, action.payload);
+
+      if (existingItem) {
+        updateExistingItem(state, action);
+        return;
+      }
+
       state.items.push(action.payload);
     },
     cartRemoveItem: (state, action: PayloadAction<RemoveItemAction>) => {
@@ -38,16 +87,13 @@ const cartSlice = createSlice({
     ) => {
       state.items = state.items.map((item) => {
         const isSelectedItem =
-          item.id === action.payload.id ||
+          item.id === action.payload.id &&
           item.selectedModifierId === action.payload.modifierId;
 
         if (isSelectedItem) {
           return {
             ...item,
-            quantity:
-              action.payload.type === "increment"
-                ? item.quantity + 1
-                : item.quantity - 1,
+            quantity: updateQuantity(item, action),
           };
         }
 
