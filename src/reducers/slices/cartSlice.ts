45485e6378e6a51029d76f554ece75d25dca8fdf
Klinger Matheus <kger.matheus@gmail.com>
2025-03-03 22:38:48 -0300

    chore: implement new actions to cart state.

--- src/reducers/slices/cartSlice.ts
+++ src/reducers/slices/cartSlice.ts
@@ -1,4 +1,10 @@
-import { CartState, Item } from "@/types";
+import {
+  CartItem,
+  CartState,
+  Item,
+  RemoveItemAction,
+  UpdateQuantityAction,
+} from "@/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: CartState = {
@@ -10,17 +16,43 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    cartAddNewItem: (state, action: PayloadAction<Item>) => {
+    cartAddNewItem: (state, action: PayloadAction<CartItem>) => {
       state.items.push(action.payload);
     },
-    cartRemoveItem: (state, action: PayloadAction<Item>) => {
-      state.items.push(action.payload);
+    cartRemoveItem: (state, action: PayloadAction<RemoveItemAction>) => {
+      state.items = state.items.filter((item) => {
+        const { id, modifierId } = action.payload;
+
+        return item.id !== id || item.selectedModifierId !== modifierId;
+      });
     },
     cartRemoveAllItems: (state) => {
       state.items = [];
     },
     cartSetSelectedItem: (state, action: PayloadAction<Item>) => {
       state.selectedItem = action.payload;
+    },
+    updateCartItemQuantity: (
+      state,
+      action: PayloadAction<UpdateQuantityAction>
+    ) => {
+      state.items = state.items.map((item) => {
+        const isSelectedItem =
+          item.id === action.payload.id ||
+          item.selectedModifierId === action.payload.modifierId;
+
+        if (isSelectedItem) {
+          return {
+            ...item,
+            quantity:
+              action.payload.type === "increment"
+                ? item.quantity + 1
+                : item.quantity - 1,
+          };
+        }
+
+        return item;
+      });
     },
   },
 });
@@ -30,6 +62,7 @@ export const {
   cartRemoveItem,
   cartRemoveAllItems,
   cartSetSelectedItem,
+  updateCartItemQuantity,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
