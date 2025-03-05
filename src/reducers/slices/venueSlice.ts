7aea4fe5768e2dd5eed6087a556e41bbc7400599
Klinger Matheus <kger.matheus@gmail.com>
2025-03-05 09:24:30 -0300

    fix: fixes import

--- src/reducers/slices/venueSlice.ts
+++ src/reducers/slices/venueSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { VenueData, VenueState } from "types";
+import { VenueData, VenueState } from "../../types";
 
 const initialState: VenueState = {
   data: null,
