a4df4a51605139ba64d3ae4091bf8cc146b787f7
Klinger Matheus <kger.matheus@gmail.com>
2025-03-05 07:47:59 -0300

    fix: fixes banner imagei in header style.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,9 @@
-import BurgerHeader from "../../assets/burger-header.webp";
-import BurgerLogo from "../../assets/burger-logo.webp";
-
 import "./header.css";
 
 export const Header = () => {
   return (
     <div className="header-container">
-      <div className="overlay-background" />
-      <img
-        className="header"
-        src={BurgerHeader}
-        alt="Burger Header"
-        loading="lazy"
-      />
-      <img className="logo" src={BurgerLogo} alt="Burger Logo" loading="lazy" />
+      <img id="banner-image" alt="Banner image" loading="lazy" />
     </div>
   );
 };
